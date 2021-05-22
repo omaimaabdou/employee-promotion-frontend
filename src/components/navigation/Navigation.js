4feb3308cc79008dcbf21ea181de7998fd32dcc7
Hajjioui-MH <hajjioui.mh@gmail.com>
2021-05-22 16:56:29 +0100

    adding logo to register page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -21,7 +21,14 @@ const Navigation = ({path})=>{
 			</nav>
 		)
 	}else if( path == "register" ){
-		return <nav className='' style={{display : 'flex',justifyContent:'flex-end'}}>
+		return <nav className='' style={{display : 'flex',justifyContent:'space-between'}} >
+			<div className="" style={{'marginTop':'2px', 'lineHeight': '0.2',display : 'flex',justifyContent:'space-between','width':'52%','padding':'0 4px','margin':'0px','alignItems':'center'}} >
+					<div className="logo-div"> <img src={Logo} alt=""/> </div>
+					<div>
+						<p>Royaume de Maroc</p>
+						<p>Ministère du Tourisme, de l'Artisanat, du Transport Aérien et de l'Economie Sociale</p>
+					</div>
+			</div>
 			<Link to="/signin" >
 				<p className='f3 link dim black underline pa3 pointer'>Sign In</p>
 			</Link>
