b557425b4e10856338ca314e1771a8923ff9d4b8
Hajjioui-MH <hajjioui.mh@gmail.com>
2021-05-22 03:49:40 +0100

    adding logo

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import Logo from "./../../images/logo_v1.png"
 
 const Navigation = ({path})=>{
 	if (path == '/') {
 		return(
-			<nav className='' style={{display : 'flex',justifyContent:'flex-end'}}>
+			<nav className='' style={{display : 'flex',justifyContent:'space-between'}}>
+				<div className="" style={{'marginTop':'2px', 'lineHeight': '0.2',display : 'flex',justifyContent:'space-between','width':'52%','padding':'0 4px','margin':'0px','alignItems':'center'}} >
+					<div className="logo-div"> <img src={Logo} alt=""/> </div>
+					<div>
+						<p>Royaume de Maroc</p>
+						<p>Ministère du Tourisme, de l'Artisanat, du Transport Aérien et de l'Economie Sociale</p>
+					</div>
+				</div>
 				<Link to="/signin" >
 					<p className='f3 link dim black underline pa3 pointer'
 					onClick={()=> localStorage.clear()}
@@ -20,7 +28,13 @@ const Navigation = ({path})=>{
 		</nav>
 	}
 	else
-		return ''
+		return <div className="" style={{'marginTop':'2px', 'lineHeight': '0.2'}} >
+			<div className="logo-div"> <img src={Logo} alt=""/> </div>
+			<div>
+				<p>Royaume de Maroc</p>
+				<p>Ministère du Tourisme, de l'Artisanat, du Transport Aérien et de l'Economie Sociale</p>
+			</div>
+		</div>
 	/*else{
 		return(
 		<nav className='' style={{display : 'flex',justifyContent:'flex-end'}}>
